ae6939855078b334f5015809ed23129fda860162
abdullahnettoor <abdullahnettoor@gmail.com>
2023-09-22 19:54:27 +0530

    + Added insertion to db via go

--- main.go
+++ main.go
@@ -13,97 +13,131 @@ const (
 	database = "test"
 )
 
-var directorTable = `create table directors (
-	director_uid UUID NOT NULL PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50),
+type Movie struct {
+	Id          string
+	Title       string
+	Genre       string
+	Director    string
+	Released_on string
+	Gross       float64
+}
 
-	CONSTRAINT directors_name_unique UNIQUE (first_name, last_name)
-);`
+var db *sql.DB
+var err error
 
-var movieTable = `create table movies (
-	movie_uid UUID NOT NULL PRIMARY KEY,
-	title VARCHAR(250) NOT NULL,
-	genre VARCHAR(100) NOT NULL,
-	director UUID REFERENCES directors(director_uid),
-	released_on DATE NOT NULL,
-	gross NUMERIC(7, 2),
+// var directorTable = `create table directors (
+// 	director_uid UUID NOT NULL PRIMARY KEY,
+// 	first_name VARCHAR(50) NOT NULL,
+// 	last_name VARCHAR(50),
 
-	CONSTRAINT movie_unique UNIQUE (title, genre, released_on)
-);`
+// 	CONSTRAINT directors_name_unique UNIQUE (first_name, last_name)
+// );`
+
+// var movieTable = `create table movies (
+// 	movie_uid UUID NOT NULL PRIMARY KEY,
+// 	title VARCHAR(250) NOT NULL,
+// 	genre VARCHAR(100) NOT NULL,
+// 	director UUID REFERENCES directors(director_uid),
+// 	released_on DATE NOT NULL,
+// 	gross NUMERIC(7, 2),
+
+// 	CONSTRAINT movie_unique UNIQUE (title, genre, released_on)
+// );`
+
+func insertMovie(db *sql.DB) {
+	m := Movie{}
+	fmt.Println("Enter Movie datas to insert")
+	fmt.Printf("Title\t:")
+	fmt.Scan(&m.Title)
+	fmt.Printf("Genre\t:")
+	fmt.Scan(&m.Genre)
+	fmt.Printf("Director\t:")
+	fmt.Scan(&m.Director)
+	fmt.Printf("Released_on\t:")
+	fmt.Scan(&m.Released_on)
+	fmt.Printf("Gross\t:")
+	fmt.Scan(&m.Gross)
+
+	_, err = db.Exec(`INSERT INTO movies (movie_uid, title, genre, director, released_on, gross)
+					  VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5)`, m.Title, m.Genre, m.Director, m.Released_on, m.Gross)
+	if err != nil {
+		fmt.Printf("Error inserting movie: %s", err)
+	}
+
+}
 
 func main() {
 
 	dbURI := fmt.Sprintf("host=%s port=%s database=%s sslmode=disable", host, port, database)
 
-	db, err := sql.Open("postgres", dbURI)
+	db, err = sql.Open("postgres", dbURI)
 	if err != nil {
 		fmt.Println("Error opening db:", err)
 		return
 	}
 	defer db.Close()
 
-	_, err = db.Exec(directorTable)
-	if err != nil {
-		fmt.Println("Error Creating Director Table:", err)
-		return
-	}
-	_, err = db.Exec(movieTable)
-	if err != nil {
-		fmt.Println("Error Creating Movie Table:", err)
-		return
-	}
+	// _, err = db.Exec(directorTable)
+	// if err != nil {
+	// 	fmt.Println("Error Creating Director Table:", err)
+	// 	return
+	// }
+	// _, err = db.Exec(movieTable)
+	// if err != nil {
+	// 	fmt.Println("Error Creating Movie Table:", err)
+	// 	return
+	// }
 
-	// Add movies to db
-	_, err = db.Exec(`
-	INSERT INTO movies (movie_uid, title, genre, released_on, gross) VALUES (uuid_generate_v4(), 'Tormented', 'Horror|Thriller', '2019-07-09', null);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'My Sex Life...', 'Comedy|Drama|Romance', '2012-05-17', 255.78);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Curly Top', 'Children|Musical|Romance', '2014-09-29', 771.23);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Blood for Dracula (Andy Warhol''s Dracula)', 'Horror', '2014-03-23', null);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'The Mark of Cain', 'Documentary|Drama', '2019-08-17', 184.25);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Where the Wild Things Are', 'Adventure|Children|Drama|Fantasy|IMAX', '2019-01-25', 621.09);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Labyrinth', 'Adventure|Fantasy|Musical', '2015-08-10', 565.32);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Ursul', 'Comedy|Drama', '2012-03-06', 855.56);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Fearless Hyena, The (Xiao quan guai zhao)', 'Action|Comedy', '2014-12-19', 507.27);
-	insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Lady Dragon', 'Action|Drama', '2010-09-26', null);
-	`)
-	if err != nil {
-		fmt.Println("Error Inserting Movies:", err)
-		return
-	}
+	// // Add movies to db
+	// _, err = db.Exec(`
+	// INSERT INTO movies (movie_uid, title, genre, released_on, gross) VALUES (uuid_generate_v4(), 'Tormented', 'Horror|Thriller', '2019-07-09', null);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'My Sex Life...', 'Comedy|Drama|Romance', '2012-05-17', 255.78);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Curly Top', 'Children|Musical|Romance', '2014-09-29', 771.23);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Blood for Dracula (Andy Warhol''s Dracula)', 'Horror', '2014-03-23', null);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'The Mark of Cain', 'Documentary|Drama', '2019-08-17', 184.25);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Where the Wild Things Are', 'Adventure|Children|Drama|Fantasy|IMAX', '2019-01-25', 621.09);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Labyrinth', 'Adventure|Fantasy|Musical', '2015-08-10', 565.32);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Ursul', 'Comedy|Drama', '2012-03-06', 855.56);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Fearless Hyena, The (Xiao quan guai zhao)', 'Action|Comedy', '2014-12-19', 507.27);
+	// insert into movies (movie_uid, title, genre, released_on, gross) values (uuid_generate_v4(), 'Lady Dragon', 'Action|Drama', '2010-09-26', null);
+	// `)
+	// if err != nil {
+	// 	fmt.Println("Error Inserting Movies:", err)
+	// 	return
+	// }
 
-	// Add directors to db
-	_, err = db.Exec(`
-	insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Theo', 'Geffcock');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Korie', 'Sayre');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Darrel', 'Yakob');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Sybilla', 'Coulling');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Kyle', 'Gronow');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lotty', 'Gerrens');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Nydia', 'Barracks');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Jerrine', 'Kinnaird');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Page', 'Insworth');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Kelcie', 'Toppin');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Wayne', null);
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Keri', 'Jozefczak');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Kerri', 'Logg');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Chadwick', 'Marco');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lars', 'Vyse');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Alysia', 'Bramsom');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lynnet', null);
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Bell', 'McVicker');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Etty', 'Gerrelt');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Candide', 'De Brett');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Griffy', 'Murney');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Nickie', 'Sokale');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Natale', 'Lacheze');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lorinda', 'Wapple');
-insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Charmaine', 'Kinnach');
-	`)
-	if err != nil {
-		fmt.Println("Error Inserting Directors:", err)
-		return
-	}
+	// // Add directors to db
+	// _, err = db.Exec(`
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Theo', 'Geffcock');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Korie', 'Sayre');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Darrel', 'Yakob');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Sybilla', 'Coulling');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Kyle', 'Gronow');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lotty', 'Gerrens');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Nydia', 'Barracks');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Jerrine', 'Kinnaird');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Page', 'Insworth');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Kelcie', 'Toppin');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Wayne', null);
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Keri', 'Jozefczak');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Kerri', 'Logg');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Chadwick', 'Marco');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lars', 'Vyse');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Alysia', 'Bramsom');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lynnet', null);
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Bell', 'McVicker');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Etty', 'Gerrelt');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Candide', 'De Brett');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Griffy', 'Murney');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Nickie', 'Sokale');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Natale', 'Lacheze');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Lorinda', 'Wapple');
+	// insert into directors (director_uid, first_name, last_name) values (uuid_generate_v4(), 'Charmaine', 'Kinnach');
+	// `)
+	// if err != nil {
+	// 	fmt.Println("Error Inserting Directors:", err)
+	// 	return
+	// }
 
 	// Update movies with random directors
 	_, err = db.Exec(`UPDATE movies
@@ -118,7 +152,18 @@ insert into directors (director_uid, first_name, last_name) values (uuid_generat
 		return
 	}
 
-	rows, err := db.Query("SELECT movie_uid, title, genre FROM movies LIMIT 10")
+	insertMovie(db)
+
+	rows, err := db.Query(`
+	SELECT
+    ROW_NUMBER() OVER (ORDER BY movies.movie_uid) AS no,
+    movies.title,
+    CONCAT(directors.first_name, ' ', directors.last_name) AS director_name,
+    movies.released_on,
+    COALESCE(movies.gross, 0)
+	FROM movies
+	JOIN directors ON movies.director = directors.director_uid;
+`)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -126,13 +171,13 @@ insert into directors (director_uid, first_name, last_name) values (uuid_generat
 	defer rows.Close()
 
 	for rows.Next() {
-		var id, title, genre string
-		err := rows.Scan(&id, &title, &genre)
+		var no, title, director, released_on, gross string
+		err := rows.Scan(&no, &title, &director, &released_on, &gross)
 		if err != nil {
 			fmt.Println("Error fetching results:", err)
 			return
 		}
-		fmt.Printf("Id: %s\tName: %s\t DOB: %s\n", id, title, genre)
+		fmt.Printf("Movie %s: %sReleased on: %s\t Director: %s\t \t Gross: %s\n", no, title, released_on, director, gross)
 	}
 
 	fmt.Println("----END----")
